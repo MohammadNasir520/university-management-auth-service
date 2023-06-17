3c8025467ad7455cf01c1589fc1182bfd23683a5
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-17 23:15:53 +0600

    server and app setup wit .env

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function bootstrap() {
+  try {
+    mongoose.connect(config.database_url as string);
+    console.log("database connect successfully");
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log("fail to connect database", err);
+  }
+}
+bootstrap();
