e3ba76b80e572cb3f6eebc897bc789befe2e8ad9
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-19 11:08:50 +0600

    created user service and router

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from 'express'
+import userRouter from '../src/app/modules/users/users.route'
+
 import cors from 'cors'
 const app: Application = express()
 
@@ -6,7 +8,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+app.use('/api/v1/users', userRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('university management server is running')
 })
 
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     console.log('database connect successfully')
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      console.log(`server is running on port ${config.port}`)
     })
   } catch (err) {
     console.log('fail to connect database', err)
