8b4126f48eb895ceaea19678ace0260ef7abd1db
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-26 00:48:08 +0600

    creating student update zodSchema

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import { StudentController } from './student.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { StudentValidation } from './student.validation';
 
 const router = express.Router();
 
 router.get('/:id', StudentController.getSingleStudent);
 router.get('/', StudentController.getAllStudents);
 router.delete('/:id', StudentController.deleteStudent);
-// router.patch(
-//   '/create-student',
-//   validateRequest(UserValidation.createUserZodSchema),
-//   UserController.createStudent
-// );
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.updateStudentZodSchema),
+  StudentController.updateStudent
+);
 
 export const StudentsRoutes = router;
