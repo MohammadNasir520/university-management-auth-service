1dbc013adb689b8e3dae773fe2f53c47c3f3d755
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-26 00:19:31 +0600

    get students done

---
+++ src/app/modules/student/student.service.ts
@@ -0,0 +1,129 @@
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { IGenericResponse } from '../../../interfaces/common';
+import { paginationHelper } from '../../helpers/paginationHelper';
+import { SortOrder } from 'mongoose';
+import { IStudent, IStudentFilters } from './student.interface';
+import { Student } from './student.model';
+import { studentSearchableFields } from './student.constant';
+
+// service
+
+const getAllStudents = async (
+  filters: IStudentFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IStudent[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+
+  // const andConditions = [
+  //   {
+  //     $or: [
+  //       {
+  //         title: {
+  //           $regex: searchTerm,
+  //           $options: 'i',
+  //         },
+  //       },
+  //       {
+  //         code: {
+  //           $regex: searchTerm,
+  //           $options: 'i',
+  //         },
+  //       },
+  //       {
+  //         year: {
+  //           $regex: searchTerm,
+  //           $options: 'i',
+  //         },
+  //       },
+  //     ],
+  //   },
+  // ];
+
+  //   const StudentSearchableFields = ['title', 'code', 'year'];
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: studentSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+  const whereCondition =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelper.calculationPagination(paginationOptions);
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+
+  const result = await Student.find(whereCondition)
+    .populate('academicSemester')
+    .populate('academicDepartment')
+    .populate('academicFaculty')
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Student.countDocuments(whereCondition);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const getSingleStudent = async (id: string): Promise<IStudent | null> => {
+  const result = await Student.findById(id)
+    .populate('academicSemester')
+    .populate('academicDepartment')
+    .populate('academicFaculty');
+  console.log('id', id, 'result', result);
+  return result;
+};
+
+const updateStudent = async (id: string, payload: Partial<IStudent>) => {
+  const result = await Student.findOneAndUpdate(
+    { _id: id },
+    {
+      $set: payload,
+    },
+    { upsert: true, new: true }
+  );
+
+  return result;
+};
+
+const deleteStudent = async (id: string) => {
+  const result = await Student.findByIdAndDelete(id)
+    .populate('academicSemester')
+    .populate('academicDepartment')
+    .populate('academicFaculty');
+  return result;
+};
+
+export const StudentService = {
+  getAllStudents,
+  getSingleStudent,
+  updateStudent,
+  deleteStudent,
+};
