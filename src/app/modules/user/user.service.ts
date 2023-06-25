d8e849842ca48ca2d32f626bbd81371a31942de0
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-25 21:58:09 +0600

    student  create successfull

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,31 +3,99 @@ import { IUser } from './user.interface';
 import config from '../../../config';
 
 import ApiError from '../../../errors/ApiError';
+// import { generateStudentId } from './user.utils';
+import { IStudent } from '../student/student.interface';
+import { AcademicSemester } from '../academicSemister/academicSemester.model';
 import { generateStudentId } from './user.utils';
+import mongoose from 'mongoose';
+import { Student } from '../student/student.model';
+import httpStatus from 'http-status';
+import { IAcademicSemester } from '../academicSemister/academicSemister.interface';
 
-const academicSemester = {
-  code: '01',
-  year: '2025',
-};
-
-const createUser = async (user: IUser): Promise<IUser | null> => {
-  // auto generated incremental Id
-  const id = await generateStudentId(academicSemester);
-
-  user.id = id;
-
+const createStudent = async (
+  student: IStudent,
+  user: IUser
+): Promise<IUser | null> => {
   // default password
   if (!user.password) {
-    user.password = config.default_user_pass as string;
+    user.password = config.default_student_pass as string;
   }
 
-  const createdUser = await User.create(user);
-  if (!createdUser) {
-    throw new ApiError(400, 'Failed to create user');
+  // set role
+  user.role = 'student';
+
+  const academicSemester = await AcademicSemester.findById(
+    student.academicSemester
+  ).lean();
+
+  // transaction and rollback
+
+  let newUserAllData = null;
+
+  const session = await mongoose.startSession();
+  try {
+    // auto generated incremental Id
+    session.startTransaction();
+    const id = await generateStudentId(academicSemester as IAcademicSemester);
+    user.id = id;
+    student.id = id;
+
+    const newStudent = await Student.create([student], { session });
+    if (!newStudent.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'failed to create student');
+    }
+
+    user.student = newStudent[0]._id;
+    const newUser = await User.create([user], { session });
+
+    if (!newUser.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
+
+    newUserAllData = newUser[0];
+
+    await session.commitTransaction();
+    await session.endSession();
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
-  return createdUser;
+
+  if (newUserAllData) {
+    // newUserAllData = await User.findOne({ id: newUserAllData.id }).populate({
+    //   path: 'student,',
+    //   populate: [
+    //     {
+    //       path: 'academicSemester',
+    //     },
+
+    //     {
+    //       path: 'academicDepartment',
+    //     },
+    //     {
+    //       path: 'academicFaculty',
+    //     },
+    //   ],
+    // });
+    newUserAllData = await User.findOne({ id: newUserAllData.id }).populate({
+      path: 'student',
+      populate: [
+        {
+          path: 'academicSemester',
+        },
+        {
+          path: 'academicDepartment',
+        },
+        {
+          path: 'academicFaculty',
+        },
+      ],
+    });
+  }
+  return newUserAllData;
 };
 
 export const UserService = {
-  createUser,
+  createStudent,
 };
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -8,26 +8,35 @@ import { bloodGroup, gender } from '../student/student.constant';
 const createUserZodSchema = z.object({
   body: z.object({
     password: z.string().optional(),
+
     student: z.object({
       name: z.object({
         firstName: z.string({
-          required_error: 'First Name is required',
+          required_error: 'First name is required',
+        }),
+        lastName: z.string({
+          required_error: 'Last name is required',
         }),
         middleName: z.string().optional(),
-        lastName: z.string({
-          required_error: 'Last Name is required',
-        }),
-      }),
-
-      dateOfBirth: z.string({
-        required_error: 'Date of Birth is required',
       }),
       gender: z.enum([...gender] as [string, ...string[]], {
-        required_error: 'gender is required and  should be male or female',
+        required_error: 'Gender is required',
       }),
-      bloodGroup: z.enum([...bloodGroup] as [string, ...string[]], {
-        required_error: 'bloodGroup is required',
+      dateOfBirth: z.string({
+        required_error: 'Date of birth is required',
       }),
+      email: z
+        .string({
+          required_error: 'Email is required',
+        })
+        .email(),
+      contactNo: z.string({
+        required_error: 'Contact number is required',
+      }),
+      emergencyContactNo: z.string({
+        required_error: 'Emergency contact number is required',
+      }),
+      bloodGroup: z.enum([...bloodGroup] as [string, ...string[]]).optional(),
       presentAddress: z.string({
         required_error: 'Present address is required',
       }),
